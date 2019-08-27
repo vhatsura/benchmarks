3bdc62076b2477ce8e5c0477ada0b25f3050496d
Vadim Hatsura <vadimgatsura@gmail.com>
2019-08-27 16:41:26 +0300

    add benchmarks for FirstToLower

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,27 @@
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace Benchmarks
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = DefaultConfig.Instance
+                .With(Job.Default.With(Runtime.Clr).With(CsProjClassicNetToolchain.Net472))
+                .With(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.NetCoreApp21))
+                .With(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.NetCoreApp22))
+                .With(Job.Default.With(Runtime.Core).With(CsProjCoreToolchain.NetCoreApp30))
+                .With(MemoryDiagnoser.Default)
+                .With(MemoryDiagnoser.Default);
+            
+            BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args, config);
+        }
+    }
+}
---
+++ src/Benchmarks/Strings/FirstToLowerBenchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using Bogus;
+
+namespace Benchmarks.Strings
+{
+    public class FirstToLowerBenchmark
+    {
+        public string text;
+
+        [Params(5, 10, 20)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var dataSet = new DataSet();
+            @text = dataSet.Random.String(length: N);
+        }
+
+        [Benchmark]
+        public string StringFormat() => $"{text[0].ToString().ToLowerInvariant()}{text.Substring(1)}";
+
+        [Benchmark]
+        public string CharToLowerWithSubstring() => char.ToLowerInvariant(text[0]) + text.Substring(1);
+
+        [Benchmark]
+        public string CharArray()
+        {
+            var charArray = text.ToCharArray();
+            charArray[0] = char.ToLower(charArray[0]);
+            return new string(charArray);
+        }
+    }
+}
